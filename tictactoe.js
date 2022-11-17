70017fd095435c110f6aa0f3afbc5e7cf3acb2e8
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-11-17 15:24:29 +0000

    Added gameBoard module for the board array

--- tictactoe.js
+++ tictactoe.js
@@ -1,9 +1,35 @@
+"use strict";
+
 const Player = (sign) => {
   this.sign = sign;
 
-  getSign: () => {
+  const getSign = () => {
     return sign;
   };
 
   return { getSign };
 };
+
+const gameBoard = (() => {
+  const board = Array(9);
+
+  const setField = (index, sign) => {
+    board[index] = sign;
+  };
+
+  const getField = (index) => {
+    return board[index];
+  };
+
+  const reset = () => {
+    board.forEach((element, i, boardArray) => {
+      boardArray[i] = "";
+    });
+  };
+
+  return { setField, getField, reset };
+})();
+
+const displayController = (() => {
+  const fieldElements = document.querySelectorAll();
+})();
