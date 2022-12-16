f8e511c1f4a4f387d12d0ee3dfe92a3f683010a2
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-16 22:17:19 +0000

    Finalized game logic/win conditions

--- tictactoe.js
+++ tictactoe.js
@@ -11,67 +11,125 @@ const Player = (sign) => {
 };
 
 const gameBoard = (() => {
-  const board = Array(9);
+  const board = ["", "", "", "", "", "", "", "", ""];
 
   const setField = (index, sign) => {
+    if (index > board.length) return;
     board[index] = sign;
   };
 
   const getField = (index) => {
+    if (index > board.length) return;
     return board[index];
   };
 
   const reset = () => {
-    board.forEach((element, i, boardArray) => {
-      boardArray[i] = "";
-    });
+    for (let i = 0; i < board.length; i++) {
+      board[i] = "";
+    }
   };
 
   return { setField, getField, reset };
 })();
 
 const displayController = (() => {
-  const fieldElements = document.querySelectorAll(".board-item");
+  const fieldElements = document.querySelectorAll(".field");
   const messageElement = document.getElementById("message");
   const restartButton = document.getElementById("restart-button");
 
-  fieldElements.forEach((field) => {
-    field.addEventListener("click", handleClick);
+  fieldElements.forEach((field) =>
+    field.addEventListener("click", (e) => {
+      if (gameController.getIsOver() || e.target.textContent !== "") return;
+      gameController.playRound(parseInt(e.target.dataset.index));
+      updateGameboard();
+    })
+  );
+
+  restartButton.addEventListener("click", (e) => {
+    gameBoard.reset();
+    gameController.reset();
+    updateGameboard();
+    setMessageElement("Player X's turn");
   });
 
-  restartButton.addEventListener("click", restartClick);
-
-  const handleClick = (e) => {
-    if (gameController.getIsOver() || e.target.textContent !== "") return;
-    gameController.playRound(parseInt(e.target.dataset.index));
-    updateGameboard();
-  };
-
-  const restartClick = () => {
-    gameBoard.reset;
-    gameController.reset;
-    updateGameboard();
-  };
-
   const updateGameboard = () => {
-    fieldElements.forEach((element, i, fieldArray) => {
-      fieldArray[i].textContent = gameBoard.getField[i];
-    });
+    for (let i = 0; i < fieldElements.length; i++) {
+      fieldElements[i].textContent = gameBoard.getField(i);
+    }
+  };
+
+  const setResultMessage = (winner) => {
+    if (winner === "Draw") {
+      setMessageElement("It's a draw!");
+    } else {
+      setMessageElement(`Player ${winner} has won!`);
+    }
   };
 
   const setMessageElement = (message) => {
     messageElement.textContent = message;
   };
 
-  const setResultsMessage = (winner) => {
-    if (winner === "Draw") {
-      setMessageElement("It's a draw!");
-    } else {
-      setMessageElement(`Player ${winner} wins the game!`);
-    }
-  };
-
-  return { setMessageElement, setResultsMessage };
+  return { setResultMessage, setMessageElement };
 })();
 
-const gameController = (() => {})();
+const gameController = (() => {
+  const playerX = Player("X");
+  const playerO = Player("O");
+  let round = 1;
+  let isOver = false;
+
+  const playRound = (fieldIndex) => {
+    gameBoard.setField(fieldIndex, getCurrentPlayerSign());
+    if (checkWinner(fieldIndex)) {
+      displayController.setResultMessage(getCurrentPlayerSign());
+      isOver = true;
+      return;
+    }
+    if (round === 9) {
+      displayController.setResultMessage("Draw");
+      isOver = true;
+      return;
+    }
+    round++;
+    displayController.setMessageElement(
+      `Player ${getCurrentPlayerSign()}'s turn`
+    );
+  };
+
+  const getCurrentPlayerSign = () => {
+    return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
+  };
+
+  const checkWinner = (fieldIndex) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    return winConditions
+      .filter((combination) => combination.includes(fieldIndex))
+      .some((possibleCombination) =>
+        possibleCombination.every(
+          (index) => gameBoard.getField(index) === getCurrentPlayerSign()
+        )
+      );
+  };
+
+  const getIsOver = () => {
+    return isOver;
+  };
+
+  const reset = () => {
+    round = 1;
+    isOver = false;
+  };
+
+  return { playRound, getIsOver, reset };
+})();
